0dba9ef06a6080bf3d65cbef642fe81e1cc14718
syk-f <syk.6412work@gmail.com>
2020-11-24 00:59:08 +0900

    Revert "データベース設計(改)"
    This reverts commit 205c79fff60280e29e13aad2fb31a487ae2fc89e.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-
-  def index
-  end
-  
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '1234'
-    end
-  end
-
 end
